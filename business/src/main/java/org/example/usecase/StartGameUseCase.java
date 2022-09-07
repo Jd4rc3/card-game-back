67b0e18b184f248d109462d3996410742a9508b9
Jd4rc3 <jdarce91@misena.edu.co>
2022-09-07 15:10:20 -0500

    StartGameUseCase

--- business/src/main/java/org/example/usecase/StartGameUseCase.java
+++ business/src/main/java/org/example/usecase/StartGameUseCase.java
@@ -1,5 +1,31 @@
 package org.example.usecase;
 
-public class StartGameUseCase {
+import co.com.sofka.domain.generic.DomainEvent;
+import org.example.game.Game;
+import org.example.game.commands.StartGameCommand;
+import org.example.game.values.GameId;
+import org.example.gateway.GameDomainEventRepository;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
+public class StartGameUseCase extends UseCaseForCommand<StartGameCommand> {
+
+  private final GameDomainEventRepository repository;
+
+  public StartGameUseCase(GameDomainEventRepository repository) {
+    this.repository = repository;
+  }
+
+  @Override
+  public Flux<DomainEvent> apply(Mono<StartGameCommand> startGameCommandMono) {
+    return startGameCommandMono.flatMapMany(
+        command -> repository.getEventsBy(command.getGameId())
+            .collectList()
+            .flatMapIterable(event -> {
+              var game = Game.from(GameId.of(command.getGameId()), event);
+              game.createBoard();
+
+              return game.getUncommittedChanges();
+            }));
+  }
 }
