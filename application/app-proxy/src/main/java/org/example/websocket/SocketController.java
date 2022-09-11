1e6ddf318296c2af377ecd2776e8d03f285fc120
Jd4rc3 <jdarce91@misena.edu.co>
2022-09-11 00:16:25 -0500

    Socket basic controller Socket basic controller implemented and EventSerializer

---
+++ application/app-proxy/src/main/java/org/example/websocket/SocketController.java
@@ -0,0 +1,74 @@
+package org.example.websocket;
+
+import co.com.sofka.domain.generic.DomainEvent;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.concurrent.ConcurrentHashMap;
+import javax.websocket.OnClose;
+import javax.websocket.OnError;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.PathParam;
+import javax.websocket.server.ServerEndpoint;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@ServerEndpoint("/retrieve/{correlationId}")
+public class SocketController {
+
+  private static Map<String, Map<String, Session>> sessions;
+
+  private GsonEventSerializer serialize;
+
+  public SocketController() {
+    if (Objects.isNull(sessions)) {
+      sessions = new ConcurrentHashMap<>();
+    }
+  }
+
+  @OnOpen
+  public void onOpen(Session session, @PathParam("correlationId") String correlationId) {
+    log.info("Connect by correlationId: {}", correlationId);
+
+    var map = sessions.getOrDefault(correlationId, new HashMap<>());
+    map.put(session.getId(), session);
+    sessions.put(correlationId, map);
+  }
+
+  @OnClose
+  public void onClose(Session session, @PathParam("correlationId") String correlationId) {
+    sessions.get(correlationId).remove(session.getId());
+
+    log.info("Disconnect by correlationId: {}", correlationId);
+  }
+
+  @OnError
+  public void onError(Session session, @PathParam("correlationId") String correlationId,
+      Throwable throwable) {
+    sessions.get(correlationId).remove(session.getId());
+    log.info(throwable.getMessage());
+  }
+
+  public void send(String correlationId, DomainEvent event) {
+    var message = serialize.serialize(event);
+
+    if (Objects.nonNull(correlationId) && sessions.containsKey(correlationId)) {
+      log.info("send from {}", correlationId);
+
+      sessions.get(correlationId).values()
+          .forEach(session -> {
+
+            try {
+              session.getAsyncRemote().sendText(message);
+            } catch (RuntimeException e) {
+              log.error(e.getMessage(), e);
+            }
+
+          });
+    }
+  }
+
+}
