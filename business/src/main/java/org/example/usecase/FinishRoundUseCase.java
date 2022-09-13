e48c836edfad080a680a4f03efc93ce93dcff1a3
Jd4rc3 <jdarce91@misena.edu.co>
2022-09-13 08:53:07 -0500

    FinishRoundUseCase implemented

--- business/src/main/java/org/example/usecase/FinishRoundUseCase.java
+++ business/src/main/java/org/example/usecase/FinishRoundUseCase.java
@@ -1,9 +1,15 @@
 package org.example.usecase;
 
 import co.com.sofka.domain.generic.DomainEvent;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
 import org.example.game.Game;
 import org.example.game.commands.FinishRoundCommand;
+import org.example.game.values.Card;
 import org.example.game.values.GameId;
+import org.example.game.values.PlayerId;
 import org.example.gateway.GameDomainEventRepository;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -22,8 +28,36 @@ public class FinishRoundUseCase extends UseCaseForCommand<FinishRoundCommand> {
         command -> repository.getEventsBy(command.getGameId().value())
             .collectList()
             .flatMapIterable(event -> {
+
               var game = Game.from(GameId.of(command.getGameId().value()), event);
-              game.finishRound(command.getBoardId(), command.getPlayers());
+              TreeMap<Integer, String> orderedGame = new TreeMap<>((t1, t2) -> t2 - t1);
+
+              Set<Card> cardsOnBoard = new HashSet<>();
+
+              game.board().match().forEach((playerId, cards) -> {
+
+                cards.stream()
+                    .map(c -> c.value().power())
+                    .reduce(Integer::sum)
+
+                    .ifPresent(points -> {
+                      orderedGame.put(points, playerId.value());
+                      cardsOnBoard.addAll(cards);
+                    });
+
+              });
+
+              var playersInGame = orderedGame.values()
+                  .stream()
+                  .map(PlayerId::of)
+                  .collect(Collectors.toSet());
+
+              var match = orderedGame.firstEntry();
+              var winnerId = match.getValue();
+              var points = match.getKey();
+
+              game.asignCardsToPlayer(PlayerId.of(winnerId), points, cardsOnBoard);
+              game.finishRound(game.board().identity(), playersInGame);
 
               return game.getUncommittedChanges();
             }));
